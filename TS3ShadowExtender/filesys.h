8f0c2ff8599c1e77672a1d1dcd7109e023eb5671
LazyDuchess <42678262+LazyDuchess@users.noreply.github.com>
2022-01-21 22:23:47 -0300

    Hopefully figured out the static library linking...

--- TS3ShadowExtender/filesys.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace FileSys
-{
-	std::wstring GetModuleDirectory();
-	std::wstring GetAbsolutePathAuto(std::wstring path);
-	bool Exists(const std::wstring& name);
-}
--- TS3ShadowExtender/stringhelper.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-std::wstring ToLowerString(std::wstring str);
-std::wstring TrimString(std::wstring str, bool removeQuotes);
